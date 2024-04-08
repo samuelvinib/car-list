c251f389d88dadb963d92620af5640f30aa8e4a9
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-04-08 15:42:22 -0300

    chore: migration e model criado para a tabelo de carros.

--- start.bat
+++ start.bat
@@ -9,5 +9,9 @@ docker-compose up -d
 REM Executa o composer install dentro do contêiner da aplicação
 docker-compose exec php composer install
 
+REM Execute o comando de migrations para criar as tabelas
+docker-compose exec php php artisan migrate
+
 REM Informa que o setup foi concluído
 echo Setup finalizado!
+echo Acesse: http://localhost:8000/
